9fa79ca11d4f33f747e29457694a37c94b9d67ca
omandagonde99 <omandagonde99@gmail.com>
2026-06-11 15:39:16 +0530

    Create Positive, Negative or Zero.cpp

---
+++ Positive, Negative or Zero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Enter the number ";
+	cin>>a;
+	
+	if(a>0){
+			cout<<"number is positive";
+		}else if(a<0){
+			cout<<"number is negative";			
+		}else{
+			cout<<"number is zero";
+			}
+			
+    return 0;
+	
+}
