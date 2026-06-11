f1971b4d0ad3fff3c1245422f19394905f1c58d5
omandagonde99 <omandagonde99@gmail.com>
2026-06-11 15:49:09 +0530

    Create Largest of Two Numbers.cpp

---
+++ Largest of Two Numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter the two number"<<endl;
+	cin>>a>>b;
+	
+	if(a>b){
+		cout<<"First number is grater";
+	}else{
+		cout<<"Secont number is grater";
+	}
+	
+	return 0;
+	
+}
