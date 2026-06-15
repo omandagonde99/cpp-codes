9bdfa87b35f1bcd6a9c7c3e3867e4e68a80d93fc
omandagonde99 <omandagonde99@gmail.com>
2026-06-15 19:08:13 +0530

    Create multiplicationtable.cpp

---
+++ multiplicationtable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int multi;
+
+    cout<<"Enter the number :";
+    cin>>n;
+
+    for(int i=1;i<=10;i++){
+           multi = n * i;
+
+           cout<< multi <<endl;
+
+    }
+
+    return 0;
+}
