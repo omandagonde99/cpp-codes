c0067353fa8d1c92c2b3e014e8281a5ad6785e18
omandagonde99 <omandagonde99@gmail.com>
2026-06-15 18:52:33 +0530

    Create sum of n number.cpp

---
+++ sum of first n all number/sum of n number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int sum = 0;
+
+    cout<<"Enter the number :";
+    cin>>n;
+
+    for(int i=0;i<=n;i++){
+        sum = sum + i ;
+    }
+
+    cout<<"total sum of natural number "<<sum;
+
+    return 0;
+}
