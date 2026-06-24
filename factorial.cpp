46fe157010ef121cecf8e35d6b66c03614e1d9bc
omandagonde99 <omandagonde99@gmail.com>
2026-06-24 10:45:09 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+
+    cin>>n;
+
+    int fac = 1;
+
+    for(int i=5;i>=1;i--){
+
+        fac = fac*i;
+
+    }
+
+    cout<< fac;
+
+    return 0;
+    
+}
