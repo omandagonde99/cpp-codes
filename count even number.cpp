d27101500b5cb9fa80c91c6d0b8ebf5a760e2586
omandagonde99 <omandagonde99@gmail.com>
2026-06-27 12:12:53 +0530

    Create count even number.cpp

---
+++ count even number.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+int main(){
+
+    std::cout<<"wellcome to count even nuber in your array :"<<std::endl;
+
+    int n;
+    std::cout<<"Enter the number`s of array :";
+
+    std::cin>>n;
+
+    int arr[100];
+    std::cout<<"Enter the element of array : ";
+
+    for(int i=0; i<n; i++){
+        std::cin>>arr[i];
+    }
+
+
+    int count = 0;
+  
+
+    for(int i=0; i<n; i++){
+        if(i%2==0){
+            count++;
+        }
+
+    }
+   
+    std::cout<<"even number is "<<count;
+
+
+    return 0;
+
+
+}
